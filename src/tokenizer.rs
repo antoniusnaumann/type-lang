7da69a9801a8d1c992c85d0a7768c9a8b7638243
Antonius Naumann <git@antonius.dev>
2024-08-11 22:33:50 +0200

    Implement tokenizer and add tests

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -28,7 +28,7 @@ impl<'a> Iterator for Tokenizer<'a> {
         }
 
         self.position = start + 1;
-        let slice = &slice[start..];
+        let slice = &self.source[start..];
         match slice.chars().nth(0) {
             Some('[') => Some(Token::BracketOpen),
             Some(']') => Some(Token::BracketClose),
@@ -39,12 +39,26 @@ impl<'a> Iterator for Tokenizer<'a> {
             Some('?') => Some(Token::QuestionMark),
             Some(',') => Some(Token::Comma),
             Some(':') => Some(Token::Colon),
-            Some(_) => todo!("Parse Ident, Type Ident and type keyword"),
+            Some(c) if c.is_alphabetic() => {
+                let end = slice
+                    .find(|c: char| !c.is_alphanumeric())
+                    .unwrap_or(slice.len());
+                let ident = &slice[..end];
+                self.position = start + end;
+
+                if ident.starts_with(|c: char| c.is_uppercase()) {
+                    Some(Token::TypeIdent(ident))
+                } else {
+                    Some(Token::Ident(ident))
+                }
+            }
+            Some(c) => todo!("Tokenizer error: {c} is not a valid token!"),
             None => None,
         }
     }
 }
 
+#[derive(Debug, PartialEq, Eq)]
 pub enum Token<'a> {
     BraceOpen,
     BraceClose,
@@ -56,8 +70,86 @@ pub enum Token<'a> {
     QuestionMark,
     Comma,
     Colon,
-    TypeKeyword,
 
     TypeIdent(&'a str),
     Ident(&'a str),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_tokenize_empty_type() {
+        let source = "
+          Example {}  
+        ";
+
+        let lexer = Tokenizer::new(source);
+        let tokens: Vec<_> = lexer.collect();
+
+        assert_eq!(
+            tokens,
+            vec![
+                Token::TypeIdent("Example"),
+                Token::BraceOpen,
+                Token::BraceClose
+            ]
+        );
+    }
+
+    #[test]
+    fn test_tokenize_list() {
+        let source = "[Object]";
+
+        let lexer = Tokenizer::new(source);
+        let tokens: Vec<_> = lexer.collect();
+
+        assert_eq!(
+            tokens,
+            vec![
+                Token::BracketOpen,
+                Token::TypeIdent("Object"),
+                Token::BracketClose
+            ]
+        )
+    }
+
+    #[test]
+    fn test_multiple_idents() {
+        let source = "Object ident otherName identWithNumber123 Type123";
+
+        let lexer = Tokenizer::new(source);
+        let tokens: Vec<_> = lexer.collect();
+
+        assert_eq!(
+            tokens,
+            vec![
+                Token::TypeIdent("Object"),
+                Token::Ident("ident"),
+                Token::Ident("otherName"),
+                Token::Ident("identWithNumber123"),
+                Token::TypeIdent("Type123")
+            ]
+        );
+    }
+
+    #[test]
+    fn test_empty_string() {
+        let source = "";
+
+        let lexer = Tokenizer::new(source);
+        let tokens: Vec<_> = lexer.collect();
+
+        assert_eq!(tokens, vec![]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_unknown_token() {
+        let source = "ident $Type";
+
+        let lexer = Tokenizer::new(source);
+        let _tokens: Vec<_> = lexer.collect();
+    }
 }
